0ec5f9dd3c37fa577d4f15e56da94ebdeef556e5
._______166 <retromac9x2@gmail.com>
2024-10-24 13:51:57 +0100

    move config to app
    i am removing the config activity from jLib to hopefully fix AOSP
    Compatibility

--- app/src/main/java/jOS/ThemeEngine/ConfigActivity.java
+++ app/src/main/java/jOS/ThemeEngine/ConfigActivity.java
@@ -1,20 +1,198 @@
 package jOS.ThemeEngine;
 
-import jOS.Core.jConfigActivity;
+import static androidx.preference.PreferenceFragmentCompat.ARG_PREFERENCE_ROOT;
+import static jOS.Core.ThemeEngine.ThemeEngine.currentTheme;
+import static jOS.Core.ThemeEngine.ThemeEngine.getThemeFromDB1;
 
-public class ConfigActivity extends jConfigActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.core.view.WindowCompat;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
+
+import java.util.Objects;
+
+import jOS.Core.utils.ErrorUtils;
+
+import jOS.Core.BuildConfig;
+import jOS.Core.jActivity;
+
+public class ConfigActivity extends jActivity
+        implements PreferenceFragmentCompat.OnPreferenceStartFragmentCallback, PreferenceFragmentCompat.OnPreferenceStartScreenCallback {
+
+    public static final String EXTRA_FRAGMENT_ARGS = ":settings:fragment_args";
+
+    // Intent extra to indicate the pref-key of the root screen when opening the settings activity
+    public static final String EXTRA_FRAGMENT_ROOT_KEY = ARG_PREFERENCE_ROOT;
+
+
     @Override
-    public int preferenceFragmentValue() {
-        return R.string.config_fragment_name;
+    protected void onCreate(Bundle savedInstanceState) {
+        configure(jOS.Core.R.layout.settings_activity, false);
+        super.onCreate(savedInstanceState);
+
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+
+        Intent intent = getIntent();
+
+        if (savedInstanceState == null) {
+            Bundle args = intent.getBundleExtra(EXTRA_FRAGMENT_ARGS);
+            if (args == null) {
+                args = new Bundle();
+            }
+
+            String root = intent.getStringExtra(EXTRA_FRAGMENT_ROOT_KEY);
+            if (!TextUtils.isEmpty(root)) {
+                args.putString(EXTRA_FRAGMENT_ROOT_KEY, root);
+            }
+
+            final FragmentManager fm = getSupportFragmentManager();
+            final Fragment f = fm.getFragmentFactory().instantiate(getClassLoader(),
+                    getString(R.string.config_fragment_name));
+            f.setArguments(args);
+            // Display the fragment as the main content.
+            fm.beginTransaction().replace(jOS.Core.R.id.content_frame, f).commit();
+        }
     }
-    public static class jThemeEngineConfigFragment extends LauncherSettingsFragment {
+
+    private boolean startPreference(String fragment, Bundle args, String key) {
+        if (getSupportFragmentManager().isStateSaved()) {
+            // Sometimes onClick can come after onPause because of being posted on the handler.
+            // Skip starting new preferences in that case.
+            return false;
+        }
+        final FragmentManager fm = getSupportFragmentManager();
+        final Fragment f = fm.getFragmentFactory().instantiate(getClassLoader(), fragment);
+        if (f instanceof DialogFragment) {
+            f.setArguments(args);
+            ((DialogFragment) f).show(fm, key);
+        } else {
+            startActivity(new Intent(this, this.getClass())
+                    .putExtra(EXTRA_FRAGMENT_ARGS, args));
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onPreferenceStartFragment(
+            @NonNull PreferenceFragmentCompat preferenceFragment, Preference pref) {
+        return startPreference(pref.getFragment(), pref.getExtras(), pref.getKey());
+    }
+
+    @Override
+    public boolean onPreferenceStartScreen(@NonNull PreferenceFragmentCompat caller, PreferenceScreen pref) {
+        Bundle args = new Bundle();
+        args.putString(ARG_PREFERENCE_ROOT, pref.getKey());
+        return startPreference(getString(R.string.config_fragment_name), args, pref.getKey());
+    }
+
+
+    /**
+     * This fragment shows the preferences.
+     */
+    public static class jThemeEngineConfigFragment extends PreferenceFragmentCompat {
+
         @Override
-        public boolean isLIBConfig() {
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            initPreference(rootKey);
+        }
+
+        public void initPreference(String rootKey){
+            setPreferencesFromResource(R.xml.themeengine_prefs, rootKey);
+            addPreferencesFromResource(jOS.Core.R.xml.lib_preference);
+
+            PreferenceScreen screen = getPreferenceScreen();
+            for (int i = screen.getPreferenceCount() - 1; i >= 0; i--) {
+                Preference preference = screen.getPreference(i);
+                if (Objects.equals(preference.getKey(), "lib_category")) {
+                    PreferenceCategory category = (PreferenceCategory) preference;
+                    for (int i2 = category.getPreferenceCount() - 1; i2 >= 0; i2--) {
+                        Preference preference2 = category.getPreference(i2);
+                        if (!configPreference(preference2)) {
+                            category.removePreference(preference2);
+                        }
+                    }
+                }
+                if (!configPreference(preference)) {
+                    screen.removePreference(preference);
+                }
+            }
+            if (getActivity() != null && !TextUtils.isEmpty(getPreferenceScreen().getTitle())) {
+                getActivity().setTitle(getPreferenceScreen().getTitle());
+            }
+        }
+
+        /**
+         * Initializes a preference. This is called for every preference. Returning false here
+         * will remove that preference from the list.
+         */
+        protected boolean configPreference(Preference preference) {
+            Log.i("Preference Logging", preference.getKey());
+            switch (preference.getKey()) {
+                case "LIB":
+                    Log.i("Preference Logging", "LIB Found!!!!");
+                    preference.setOnPreferenceClickListener(p -> {
+                        Intent intent = new Intent("jOS.System.LibConfig");
+                        try {
+                            startActivity(intent);
+                        } catch (Exception e) {
+                            ErrorUtils.handle(e, requireActivity());
+                        }
+                        return false;
+                    });
+                    return false;
+                case "LIBVer":
+                    Log.i("Preference Logging", "LIBVer Found!!!!");
+                    preference.setSummary(BuildConfig.LIBVersion);
+                    return true;
+            }
             return true;
         }
+
         @Override
-        public int preferenceXML() {
-            return R.xml.themeengine_prefs;
+        public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            View listView = getListView();
+            final int bottomPadding = listView.getPaddingBottom();
+            listView.setOnApplyWindowInsetsListener((v, insets) -> {
+                v.setPadding(
+                        v.getPaddingLeft(),
+                        v.getPaddingTop(),
+                        v.getPaddingRight(),
+                        bottomPadding + insets.getSystemWindowInsetBottom());
+                return insets.consumeSystemWindowInsets();
+            });
+
+            // Overriding Text Direction in the Androidx preference library to support RTL
+            view.setTextDirection(View.TEXT_DIRECTION_LOCALE);
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+
+            if (!Objects.equals(currentTheme, getThemeFromDB1(getPreferenceManager().getContext()))) {
+                recreateActivityNow();
+            }
+        }
+
+        private void recreateActivityNow() {
+            Activity activity = getActivity();
+            if (activity != null) {
+                activity.recreate();
+            }
         }
     }
 }
+
